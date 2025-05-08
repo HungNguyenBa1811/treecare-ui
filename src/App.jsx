2e5e1d55a89c45abec6c2b46414ee3ca21a31502
HungNguyenBa1811 <nahh1811@gmail.com>
2025-05-08 13:41:05 +0700

    chore(img): update QR code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
 import { publicRoutes } from './routes/routes';
 import DefaultLayout from './layouts/DefaultLayout'
 
--- src/pages/SupportUs/SupportUs.jsx
+++ src/pages/SupportUs/SupportUs.jsx
@@ -4,16 +4,16 @@ export default function SupportUs() {
     return (
         <div className='flex flex-col items-center justify-center'>
             <div>
-            <h1 className="text-3xl font-bold mb-4 text-center text-green-800">Help Us Grow</h1>
-            <p className="text-lg mb-6 text-center max-w-md text-green-700">
-                Your donation helps us continue our mission to care for trees and the environment. Every contribution makes a difference!
-            </p>
-            <p className="text-center font-semibold mb-2 text-green-800">Scan the QR code below to donate:</p>
+                <h1 className="text-3xl font-bold mb-4 text-center text-green-800">Support Us</h1>
+                <p className="text-lg mb-6 text-center max-w-md text-green-700">
+                    Scan the QR code below to access the premium version of our app. Your support helps us continue improving our services!
+                </p>
+                <p className="text-center font-semibold mb-2 text-green-800">Scan the QR code:</p>
             </div>
             <div className='w-100'>
                 <img
                     src="./QRDonation.jpg"
-                    alt=""
+                    alt="QR code to access premium version"
                 />
             </div>
         </div>
