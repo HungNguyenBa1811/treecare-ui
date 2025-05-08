f8801b8c550edb75a4af06b81b187971681d95db
HungNguyenBa1811 <nahh1811@gmail.com>
2025-05-08 13:51:26 +0700

    fix(layout): adjust suport page layout

--- src/pages/SupportUs/SupportUs.jsx
+++ src/pages/SupportUs/SupportUs.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 
 export default function SupportUs() {
     return (
-        <div className='flex flex-col items-center justify-center'>
-            <div>
+        <div className='flex flex-col items-center justify-center py-20'>
+            <div className=''>
                 <h1 className="text-3xl font-bold mb-4 text-center text-green-800">Support Us</h1>
-                <p className="text-lg mb-6 text-center max-w-md text-green-700">
+                <p className="text-lg mb-2 text-center max-w-md text-green-700">
                     Scan the QR code below to access the premium version of our app. Your support helps us continue improving our services!
                 </p>
-                <p className="text-center font-semibold mb-2 text-green-800">Scan the QR code:</p>
             </div>
-            <div className='w-100'>
+            <div className='w-100 flex justify-center'>
                 <img
                     src="./QRDonation.png"
                     alt="QR code to access premium version"
